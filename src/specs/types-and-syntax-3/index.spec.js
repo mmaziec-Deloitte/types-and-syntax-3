534375d9da3a697dbf6f0fdcdfe0af744fb44c53
mmaziec-deloitte <mmaziec@deloittece.com>
2022-07-13 14:33:38 +0200

    Add solutions

--- src/specs/types-and-syntax-3/index.spec.js
+++ src/specs/types-and-syntax-3/index.spec.js
@@ -9,44 +9,44 @@ describe('coercion - types and syntax - 3', () => {
     test('explicit coercion', function(){
         let a = 42, b = 0, c = true, d = undefined, e = "John Lennon", f = [1,2,3], g = {name: "John"};
 
-        expect(typeof String(a)).toEqual( /* YOUR ANSWER HERE */ );
-        expect(typeof Boolean(a)).toEqual( /* YOUR ANSWER HERE */ );
-        expect(typeof Number(a)).toEqual( /* YOUR ANSWER HERE */ );
+        expect(typeof String(a)).toEqual("string");
+        expect(typeof Boolean(a)).toEqual("boolean");
+        expect(typeof Number(a)).toEqual("number");
 
-        expect(typeof String(b)).toEqual( /* YOUR ANSWER HERE */ );
-        expect(typeof Boolean(b)).toEqual( /* YOUR ANSWER HERE */ );
-        expect(typeof Number(b)).toEqual( /* YOUR ANSWER HERE */ );
+        expect(typeof String(b)).toEqual("string");
+        expect(typeof Boolean(b)).toEqual("boolean");
+        expect(typeof Number(b)).toEqual("number");
 
-        expect(typeof String(c)).toEqual( /* YOUR ANSWER HERE */ );
-        expect(typeof Boolean(c)).toEqual( /* YOUR ANSWER HERE */ );
-        expect(typeof Number(c)).toEqual( /* YOUR ANSWER HERE */ );
+        expect(typeof String(c)).toEqual("string");
+        expect(typeof Boolean(c)).toEqual("boolean");
+        expect(typeof Number(c)).toEqual("number");
 
-        expect(typeof String(d)).toEqual( /* YOUR ANSWER HERE */ );
-        expect(typeof Boolean(d)).toEqual( /* YOUR ANSWER HERE */ );
-        expect(typeof Number(d)).toEqual( /* YOUR ANSWER HERE */ );
+        expect(typeof String(d)).toEqual("string");
+        expect(typeof Boolean(d)).toEqual("boolean");
+        expect(typeof Number(d)).toEqual("number");
 
-        expect(typeof String(e)).toEqual( /* YOUR ANSWER HERE */ );
-        expect(typeof Boolean(e)).toEqual( /* YOUR ANSWER HERE */ );
-        expect(typeof Number(e)).toEqual( /* YOUR ANSWER HERE */ );
+        expect(typeof String(e)).toEqual("string");
+        expect(typeof Boolean(e)).toEqual("boolean");
+        expect(typeof Number(e)).toEqual("number");
 
-        expect(typeof String(f)).toEqual( /* YOUR ANSWER HERE */ );
-        expect(typeof Boolean(f)).toEqual( /* YOUR ANSWER HERE */ );
-        expect(typeof Number(f)).toEqual( /* YOUR ANSWER HERE */ );
+        expect(typeof String(f)).toEqual("string");
+        expect(typeof Boolean(f)).toEqual("boolean");
+        expect(typeof Number(f)).toEqual("number");
 
-        expect(typeof String(g)).toEqual( /* YOUR ANSWER HERE */ );
-        expect(typeof Boolean(g)).toEqual( /* YOUR ANSWER HERE */ );
-        expect(typeof Number(g)).toEqual( /* YOUR ANSWER HERE */ );
+        expect(typeof String(g)).toEqual("string");
+        expect(typeof Boolean(g)).toEqual("boolean");
+        expect(typeof Number(g)).toEqual("number");
     });
     test('implicit coercion', function(){
-        expect(4 * "5").toEqual( /* YOUR ANSWER HERE */ );
-        expect(3 + "5" + 1).toEqual( /* YOUR ANSWER HERE */ );
-        expect("42" + 5).toEqual( /* YOUR ANSWER HERE */ );
-        expect(true + true).toEqual( /* YOUR ANSWER HERE */ );
-        expect("John" ? 4 : 1).toEqual( /* YOUR ANSWER HERE */ );
-        expect(undefined ? 4 : 1).toEqual( /* YOUR ANSWER HERE */ );
-        expect(1 + 2 + "1").toEqual( /* YOUR ANSWER HERE */ );
-        expect((1 + "2") + 1).toEqual( /* YOUR ANSWER HERE */ );
-        expect(4 * "Lennon").toEqual( /* YOUR ANSWER HERE */ );
+        expect(4 * "5").toEqual(20);
+        expect(3 + "5" + 1).toEqual("351");
+        expect("42" + 5).toEqual("425");
+        expect(true + true).toEqual(2);
+        expect("John" ? 4 : 1).toEqual(4);
+        expect(undefined ? 4 : 1).toEqual(1);
+        expect(1 + 2 + "1").toEqual("31");
+        expect((1 + "2") + 1).toEqual("121");
+        expect(4 * "Lennon").toEqual(NaN);
     });
 })
 
@@ -62,7 +62,7 @@ describe('arithmetic operators - types and syntax - 3', () => {
             return ++a;
         }
 
-        expect(Add(a)).toEqual( /* YOUR ANSWER HERE */ );
+        expect(Add(a)).toEqual(11);
     });
 
     test('suffixed incrementation', function(){
@@ -70,7 +70,7 @@ describe('arithmetic operators - types and syntax - 3', () => {
             return a++;
         }
 
-        expect(Add(a)).toEqual( /* YOUR ANSWER HERE */ );
+        expect(Add(a)).toEqual(10);
     });
 
     test('prefixed decrementation', function(){
@@ -78,7 +78,7 @@ describe('arithmetic operators - types and syntax - 3', () => {
             return --a;
         }
 
-        expect(Dec(a)).toEqual( /* YOUR ANSWER HERE */ );
+        expect(Dec(a)).toEqual(9);
     });
 
     test('suffixed decrementation', function(){
@@ -86,14 +86,14 @@ describe('arithmetic operators - types and syntax - 3', () => {
             return a--;
         }
 
-        expect(Dec(a)).toEqual( /* YOUR ANSWER HERE */ );
+        expect(Dec(a)).toEqual(10);
     });
 
     test('modulo', function(){
-        expect(a % 2).toEqual( /* YOUR ANSWER HERE */ );
-        expect(b % 2).toEqual( /* YOUR ANSWER HERE */ );
-        expect(a % b).toEqual( /* YOUR ANSWER HERE */ );
-        expect(b % a).toEqual( /* YOUR ANSWER HERE */ );
+        expect(a % 2).toEqual(0);
+        expect(b % 2).toEqual(1);
+        expect(a % b).toEqual(10);
+        expect(b % a).toEqual(3);
     });
 })
 
@@ -101,17 +101,17 @@ describe('comparison operators - types and syntax - 3', () => {
     let a = "23", b = 23, c = 0, d = false, e = undefined;
 
     test('loose equality', function(){
-        expect(a == b).toEqual( /* YOUR ANSWER HERE */ );
-        expect(b == c).toEqual( /* YOUR ANSWER HERE */ );
-        expect(c == d).toEqual( /* YOUR ANSWER HERE */ );
-        expect(d == e).toEqual( /* YOUR ANSWER HERE */ );
+        expect(a == b).toEqual(true);
+        expect(b == c).toEqual(false);
+        expect(c == d).toEqual(true);
+        expect(d == e).toEqual(false);
     });
 
     test('strict equality', function(){
-        expect(a === b).toEqual( /* YOUR ANSWER HERE */ );
-        expect(b === c).toEqual( /* YOUR ANSWER HERE */ );
-        expect(c === d).toEqual( /* YOUR ANSWER HERE */ );
-        expect(d === e).toEqual( /* YOUR ANSWER HERE */ );
+        expect(a === b).toEqual(false);
+        expect(b === c).toEqual(false);
+        expect(c === d).toEqual(false);
+        expect(d === e).toEqual(false);
     });
 
     test('ternary operator (elvis)', function(){
@@ -122,16 +122,17 @@ describe('comparison operators - types and syntax - 3', () => {
         function Dec(a, b){
             return a - b;
         }
+        //let a = "23", b = 23, c = 0, d = false, e = undefined;
 
-        expect(a ? true : false).toEqual( /* YOUR ANSWER HERE */ );
-        expect(b ? true : false).toEqual( /* YOUR ANSWER HERE */ );
-        expect(c ? true : false).toEqual( /* YOUR ANSWER HERE */ );
-        expect(d ? true : false).toEqual( /* YOUR ANSWER HERE */ );
+        expect(a ? true : false).toEqual(true);
+        expect(b ? true : false).toEqual(true);
+        expect(c ? true : false).toEqual(false);
+        expect(d ? true : false).toEqual(false);
 
-        expect(Add(5,10) > 10 ? true : false).toEqual( /* YOUR ANSWER HERE */ );
-        expect(Dec(5,10) > 10 ? true : false).toEqual( /* YOUR ANSWER HERE */ );
-        expect(Add(3,2) > 10 ? true : false).toEqual( /* YOUR ANSWER HERE */ );
-        expect(Dec(3,2) > 10 ? true : false).toEqual( /* YOUR ANSWER HERE */ );
+        expect(Add(5,10) > 10 ? true : false).toEqual(true);
+        expect(Dec(5,10) > 10 ? true : false).toEqual(false);
+        expect(Add(3,2) > 10 ? true : false).toEqual(false);
+        expect(Dec(3,2) > 10 ? true : false).toEqual(false);
     });
 })
 
@@ -139,23 +140,23 @@ describe('logical operators - types and syntax - 3', () => {
     let a = "38", b = 38, c = 0, d = false, e = undefined;
 
     test('logical or', function(){
-        expect(a || b).toEqual( /* YOUR ANSWER HERE */ );
-        expect(b || c).toEqual( /* YOUR ANSWER HERE */ );
-        expect(c || d).toEqual( /* YOUR ANSWER HERE */ );
-        expect(d || e).toEqual( /* YOUR ANSWER HERE */ );
+        expect(a || b).toEqual("38");
+        expect(b || c).toEqual(38);
+        expect(c || d).toEqual(false);
+        expect(d || e).toEqual(undefined);
     });
 
     test('logical and', function(){
-        expect(a && b).toEqual( /* YOUR ANSWER HERE */ );
-        expect(b && c).toEqual( /* YOUR ANSWER HERE */ );
-        expect(c && d).toEqual( /* YOUR ANSWER HERE */ );
-        expect(d && e).toEqual( /* YOUR ANSWER HERE */ );
+        expect(a && b).toEqual(38);
+        expect(b && c).toEqual(0);
+        expect(c && d).toEqual(0);
+        expect(d && e).toEqual(false);
     });
 
     test('combined expressions', function(){
-        expect(a || b || c).toEqual( /* YOUR ANSWER HERE */ );
-        expect(b || c && d).toEqual( /* YOUR ANSWER HERE */ );
-        expect(c && d && e).toEqual( /* YOUR ANSWER HERE */ );
-        expect((d || e) && c || (a || b)).toEqual( /* YOUR ANSWER HERE */ );
+        expect(a || b || c).toEqual("38");
+        expect(b || c && d).toEqual(38);
+        expect(c && d && e).toEqual(0);
+        expect((d || e) && c || (a || b)).toEqual("38");
     });
 })
